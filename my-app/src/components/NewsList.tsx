8ea4df0a9b12aacfd28fc46ad485b59df0ca400c
nnadya <n.d.niki2012@gmail.com>
2025-01-10 20:32:27 +0300

    add types

--- my-app/src/components/NewsList.tsx
+++ my-app/src/components/NewsList.tsx
@@ -1,16 +1,26 @@
-// src/components/NewsList.tsx
-const NewsList = ({ news, onEdit, onDelete }) => {
+import React from "react";
+import { NewsItem } from "../types/NewsItem";
+
+interface NewsListProps {
+  news: NewsItem[];
+  onEdit: (item: NewsItem) => void;
+  onDelete: (id: number) => void;
+}
+
+const NewsList: React.FC<NewsListProps> = ({ news, onEdit, onDelete }) => {
   return (
     <div>
       <h2>Список новостей</h2>
       <ul>
-        {news.map((item) => (
-          <li key={item.id}>
-            <h3>{item.title}</h3>
-            <p>{item.content}</p>
-            <button onClick={() => onEdit(item)}>Редактировать</button>
-            <button onClick={() => onDelete(item.id)}>Удалить</button>
-          </li>
+        {news.length === 0
+          ? "Добавьте новость, и она появится здесь"
+          : news.map((item: NewsItem) => (
+            <li key={item.id}>
+              <h3>{item.title}</h3>
+              <p>{item.content}</p>
+              <button onClick={() => onEdit(item)}>Редактировать</button>
+              <button onClick={() => onDelete(item.id)}>Удалить</button>
+            </li>
         ))}
       </ul>
     </div>
