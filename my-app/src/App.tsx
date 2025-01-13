44d59314bcbec516fe39c5eb2a971bc325b2ddbb
nnadya <n.d.niki2012@gmail.com>
2025-01-14 02:09:00 +0300

    add modal window

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import NewsList from './components/NewsList';
 import NewsForm from './components/NewsForm';
+import ModalWindow from './components/ModalWindow';
 import { NewsItem } from './types/NewsItem';
 
 const App: React.FC = () => {
   const [news, setNews] = useState<NewsItem[]>([]);
   const [editingItem, setEditingItem] = useState<NewsItem | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const storedNews = localStorage.getItem('news');
@@ -18,30 +20,42 @@ const App: React.FC = () => {
     localStorage.setItem('news', JSON.stringify(news));
   }, [news]);
 
-  const handleAddOrUpdate = (item: NewsItem) => {
-    if (editingItem) {
-      setNews(news.map((n) => (n.id === item.id ? item : n)));
-      setEditingItem(null);
-    } else {
-      setNews([...news, item]);
-    }
+  const handleAdd = (item: NewsItem) => {
+    setNews([...news, item]);
+};
+
+  const handleUpdate = (item: NewsItem) => {
+    setNews(news.map((n) => (n.id === item.id ? item : n)));
+    closeModal();
   };
 
   const handleEdit = (item: NewsItem) => {
     setEditingItem(item);
+    setIsModalOpen(true);
   };
 
   const handleDelete = (id: number) => {
-    setNews(news.filter((n) => n.id !== id));
+    setNews(news.filter((item) => item.id !== id));
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setEditingItem(null);
   };
 
   return (
     <div>
       <h1>CRUD Новостей</h1>
-      <NewsForm onSubmit={handleAddOrUpdate} existingItem={editingItem} />
+      <NewsForm onSubmit={handleAdd} />
       <NewsList news={news} onEdit={handleEdit} onDelete={handleDelete} />
+      <ModalWindow 
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        editingItem={editingItem}
+        onUpdate={handleUpdate}
+      />
     </div>
   );
 };
 
-export default App
+export default App;
