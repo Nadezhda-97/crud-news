9b831429c843b7c92d7a4c35a5f6aa2d78c6658c
nnadya <n.d.niki2012@gmail.com>
2025-01-14 02:12:34 +0300

    create ModalWindow.tsx

---
+++ my-app/src/components/ModalWindow.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+import EditNewsForm from './EditNewsForm';
+import { NewsItem } from '../types/NewsItem';
+
+interface ModalWindowProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onUpdate: (item: NewsItem) => void;
+  editingItem: NewsItem | null;
+};
+
+const ModalWindow: React.FC<ModalWindowProps> = ({ isOpen, onClose, onUpdate, editingItem }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      ariaHideApp={false}
+      className="modal"
+      overlayClassName="modal-overlay"
+    >
+      <h2>Редактировать новость</h2>
+        {editingItem && (
+          <EditNewsForm
+            editingItem={editingItem}
+            onSubmit={onUpdate}
+            onClose={onClose}
+          />
+        )}
+      <button className="modal-close-button" onClick={onClose}>Закрыть</button>
+    </Modal>
+  );
+};
+
+export default ModalWindow;
